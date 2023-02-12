7223c6c8f3e0d55ae44d7c78800ce0e4013f4874
anasegor <a_n_a_s@inbox.ru>
2023-02-12 13:04:10 +0300

    git add

---
+++ Zadanie_1IB/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace Zadanie_1IB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.for_e = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog3 = new System.Windows.Forms.SaveFileDialog();
+            this.for_n = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.for_d = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(300, 210);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(324, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(300, 210);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выполнить преобразование";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // for_e
+            // 
+            this.for_e.Location = new System.Drawing.Point(712, 45);
+            this.for_e.MaxLength = 256;
+            this.for_e.Name = "for_e";
+            this.for_e.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.for_e.Size = new System.Drawing.Size(69, 20);
+            this.for_e.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(655, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сгенерировать p и q";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(298, 228);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(324, 228);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ">>";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "<<";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ввод";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(441, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "вывод";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 286);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 47);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Загрузить открытый текст";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 47);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Скачать зашифрованный текст";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(363, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 52);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Загрузить зашифрованный текст";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(468, 290);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 49);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Скачать расшифрованный текст";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "(*.txt)|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.CheckFileExists = true;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.CheckFileExists = true;
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            // 
+            // saveFileDialog3
+            // 
+            this.saveFileDialog3.CheckFileExists = true;
+            // 
+            // for_n
+            // 
+            this.for_n.Location = new System.Drawing.Point(712, 71);
+            this.for_n.MaxLength = 256;
+            this.for_n.Name = "for_n";
+            this.for_n.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.for_n.Size = new System.Drawing.Size(69, 20);
+            this.for_n.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(687, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "e=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(687, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "n=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(657, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "d";
+            // 
+            // for_d
+            // 
+            this.for_d.Location = new System.Drawing.Point(681, 164);
+            this.for_d.Name = "for_d";
+            this.for_d.Size = new System.Drawing.Size(100, 20);
+            this.for_d.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.for_d);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.for_n);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.for_e);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox for_e;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog3;
+        private System.Windows.Forms.TextBox for_n;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox for_d;
+    }
+}
+
---
+++ Zadanie_1IB/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Numerics;
+
+
+
+namespace Zadanie_1IB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int p = 0;
+        int q = 0;
+        uint n;
+        uint e;
+        uint phi_n;
+        uint d=0;
+        byte[] array;
+        uint[] array1;
+        public String EnText;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs el)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(Name);
+
+            }
+        }
+        private void button4_Click(object sender, EventArgs el)
+        {
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                Name = saveFileDialog2.FileName;
+
+                File.WriteAllText(Name, textBox2.Text);
+            }
+        }
+        private void button5_Click(object sender, EventArgs el)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                Name = openFileDialog2.FileName;
+                textBox2.Clear();
+                textBox2.Text = File.ReadAllText(Name);
+            }
+        }
+        private void button6_Click(object sender, EventArgs el)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Name = saveFileDialog1.FileName;
+                File.WriteAllText(Name, textBox1.Text);
+            }
+
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs el)
+        {
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs el)
+        {
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            button6.Enabled = true;
+
+        }
+        //static public uint InPowMod(uint x, uint y, uint n)
+        //{
+        //    if (y == 0) return 1;//выход из рекурсии
+        //    if (y % 2 == 1)//нечетное
+        //        return (x * InPowMod(x, y - 1, n)) % n;
+        //    uint temp = InPowMod(x, y / 2, n);//четное-сначала считаем y/2
+        //    return (temp * temp) % n;//затем в квадрат по модулю n
+        //}
+        static public uint InPowMod(UInt64 number, UInt64 index, UInt64 mod)
+        {
+            /*
+			uint r = 1;
+			while(y>0)
+            {
+                if (y%2 == 1)
+                {
+					r = (r * x) % n;
+                }
+				x = (x * x) % n;
+				y >>= 1;
+            }
+			return r;
+			*/
+            if (index == 0)
+                return 1;
+            UInt64 res = 1;
+            while (index > 0)
+            {
+                if (index % 2 == 1) res = checked(res * number) % mod;
+
+                index /= 2;
+                number = checked(number * number) % mod;
+            }
+
+            return (uint)res;
+        }
+        private bool testFerma(uint x)
+        {
+
+            Random rand1 = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                uint a = (uint)rand1.Next(2, (int)(x - 1));
+                if (InPowMod(a, x - 1, x) != 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        private void genP()
+        {
+            Random rnd = new Random();
+            //p = rnd.Next((int)Math.Pow(2, 4), (int)Math.Pow(2, 8));
+            p = rnd.Next((int)Math.Pow(2, 8), (int)Math.Pow(2, 16));
+            if (p % 2 == 0) p--;
+
+
+        }
+        private void genQ()
+        {
+            Random rnd = new Random();
+            //q = rnd.Next((int)Math.Pow(2, 4), (int)Math.Pow(2, 8));
+            q = rnd.Next((int)Math.Pow(2, 8), (int)Math.Pow(2, 16));
+            if (q % 2 == 0) q--;
+
+        }
+
+        private void genE()
+        {
+            Random rnd = new Random();
+           e = (uint)(  rnd.Next( -(int)Math.Pow(2, 16), (int)Math.Pow(2, 16) )    + (int)Math.Pow(2, 16) );
+            //e = (uint)(rnd.Next(0, (int)Math.Pow(2, 8)));
+
+        }
+        private uint gcd(uint a, uint b)//(рекурентный вызов)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a >= b) a = a % b;
+                else b = b % a;
+            }
+
+            return a + b;
+        }
+
+
+        //private void ExtendendEvklid(uint e, uint fee, ref uint d)
+        //{
+        //    uint a = e;
+        //    uint b = fee;
+        //    int[] y = new int[2] { 1, 0 };
+        //    int[] u = new int[2] { 0, 1 };
+
+        //    int ybuf, ubuf;
+        //    uint r;
+        //    uint q;
+        //    while (b != 0)
+        //    {
+        //        q = a / b;
+
+        //        r = a % b;
+        //        a = b;
+        //        b = r;
+
+        //        ybuf = y[1];
+        //        ubuf = u[1];
+
+        //        y[1] = y[0] - (int)(q * y[1]);
+        //        u[1] = u[0] - (int)(q * u[1]);
+
+        //        y[0] = ybuf;
+        //        u[0] = ubuf;
+        //    }
+        //    if (a == 1) d = (uint)(y[0] % fee);
+        //}
+        static public uint ExtendendEvklid(uint a, uint b)
+        {
+            uint
+                Y1 = 1,
+                Y0 = 0,
+                X1 = 0,
+                X0 = 1,
+                r, q;
+            while (b != 0)
+            {
+                uint tmp;
+                r = a % b;
+                a = b;
+                b = r;
+                if (b != 0)
+                {
+                    q = a / b;
+                }
+                else
+                {
+                    return X1;
+                }
+                tmp = Y0;
+                Y0 = Y1 - q * Y0;
+                Y1 = tmp;
+                tmp = X0;
+                X0 = X1 - q * X0;
+                X1 = tmp;
+            }
+            return X1;
+        }
+        private void button2_Click(object sender, EventArgs el)//генерация e и n
+        {
+            do
+            { genP(); }
+            while (testFerma((uint)p) == false);
+            do
+            { genQ(); }
+            while (testFerma((uint)q) == false ||q==p);
+
+            n = (uint)p * (uint)q;
+            for_n.Text = n.ToString();
+            phi_n = (uint)(p - 1) * (uint)(q - 1);
+
+            do
+            {
+                genE();
+               d=ExtendendEvklid(e, phi_n);//не правильно?
+                
+                //Gcd(e, phi_n, out w, out d);
+            } while (e>= phi_n || gcd(e, phi_n) != 1|| d==0 );
+            for_e.Text = e.ToString();
+            for_d.Text = d.ToString();
+        }
+
+        //static public string RSAEncrypt(String ParentText, String eKey, String nKey)
+        //{
+        //    UInt64 KeyE = Convert.ToUInt64(eKey);
+        //    UInt64 KeyN = Convert.ToUInt64(nKey);
+
+        //    String EncryptedText = "";
+        //    for (int i = 0; i < ParentText.Length; i++)
+        //    {
+        //        UInt64 m = Convert.ToUInt64(ParentText[i]);
+        //        uint c = (InPowMod(m, KeyE, KeyN));
+        //        EncryptedText += c.ToString() + " ";
+        //    }
+        //    return EncryptedText;
+        //}
+
+        //static public string RSADecrypt(String CipherText, String dKey, String nKey)
+        //{
+        //    UInt64 KeyD = Convert.ToUInt64(dKey);
+        //    UInt64 KeyN = Convert.ToUInt64(nKey);
+
+        //    String PlainText = "";
+        //    String[] ArrayNumbers = CipherText.TrimEnd().Split(' ');
+
+        //    for (int i = 0; i < ArrayNumbers.Length; i++)
+        //    {
+        //        UInt64 c = Convert.ToUInt64(ArrayNumbers[i]);
+        //        uint m = (InPowMod(c, KeyD, KeyN));
+        //        PlainText += Convert.ToChar(m);
+        //    }
+        //    return PlainText;
+        //}
+        public void button1_Click(object sender, EventArgs el)
+        {
+            if (radioButton2.Checked)//шифрование
+            {
+                textBox2.Text = ""; ;
+                var str = textBox1.Text;
+                array = Encoding.GetEncoding(1251).GetBytes(str);
+                array1 = new uint[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array1[i] = InPowMod(array[i], e, n);//С=M^e(mod n)
+                    textBox2.Text += Convert.ToString(array1[i]);
+                    textBox2.Text += " ";
+                }
+            }
+            else//дешифровка
+            {
+                textBox1.Clear();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = (byte)InPowMod(array1[i], d, n);
+                }
+
+                var str = Encoding.GetEncoding(1251).GetString(array);
+                textBox1.Text = str;
+            }
+        }
+       
+
+
+    }
+}
